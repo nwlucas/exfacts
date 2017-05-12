297f79b305a4ffac05ed773727f09e775a6f83ea
Nigel Williams-Lucas <yesniah@gmail.com>
2017-05-12 17:56:16 -0400

    Adding in docs

--- lib/exfacts.ex
+++ lib/exfacts.ex
@@ -1,6 +1,23 @@
 defmodule ExFacts do
   @moduledoc """
   Provides the entry point for the facts gathering and output.
+
+  The module has facilities that can collect and parse system metrics or facts
+  for a variety of different key system areas. Currently on Linux systems are
+  implemented but ultimately the tool should allow the same functionality across
+  many diffrent operating systems
+
+  ## CPU
+
+  ## Disk
+
+  ## Host
+
+  ## Load
+
+  ## Mem
+
+  ## Net
   """
 
   @doc false
