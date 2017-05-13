7a239dd601eefc85451d8e83cf8431dceb0da80d
Nigel Williams-Lucas <yesniah@gmail.com>
2017-05-13 16:10:09 -0400

    Updated version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExFacts.Mixfile do
   def project do
     [
       app: :exfacts,
-      version: "0.1.3",
+      version: "0.1.5",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
