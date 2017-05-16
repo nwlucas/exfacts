2915d42fcfdd565bef841d6f56e2b5828b163411
Nigel Williams-Lucas <yesniah@gmail.com>
2017-05-16 07:03:07 -0400

    Net started

--- lib/facts/system/net/net.ex
+++ lib/facts/system/net/net.ex
@@ -1,6 +1,7 @@
 defmodule ExFacts.System.Net do
   alias ExFacts.System.Net.InterfaceStat
   import ExFacts.Utils
+  use Bitwise
 
   @moduledoc """
   Handles all logic with regards to collecting metrics on the interfaces of the host.
@@ -28,8 +29,29 @@ defmodule ExFacts.System.Net do
       |> Enum.map(fn x -> %InterfaceStat
         {
           name: x,
-          hardware_addr: read_file(path <> "/#{x}/address") |> String.trim("\n"),
-          mtu: read_file(path <> "/#{x}/mtu") |> String.trim("\n") |> String.to_integer
+          hardware_addr: read_file(path <> "/#{x}/address") |> String.strip(),
+          mtu: read_file(path <> "/#{x}/mtu") |> String.strip |> String.to_integer(),
+          flags: get_sys_flags(x)
         } end)
+
+    is
+  end
+
+  @spec get_sys_flags(binary) :: [binary]
+  defp get_sys_flags(interface) do
+    path = host_sys("/class/net/" <> interface)
+
+    iff_flags = File.read!(path <> "/flags")
+                |> String.strip
+                |> String.trim_leading("0x")
+                |> String.to_integer(16)
+
+    flags = if (iff_flags &&& 1) != 0, do: ["UP"]
+    flags = if (iff_flags &&& 2) != 0, do: flags ++ ["BROADCAST"], else: flags ++ []
+    flags = if (iff_flags &&& 3) != 0, do: flags ++ ["LOOPBACK"], else: flags ++ []
+    flags = if (iff_flags &&& 4) != 0, do: flags ++ ["POINTTOPOINT"], else: flags ++ []
+    flags = if (iff_flags &&& 5) != 0, do: flags ++ ["MULTICAST"], else: flags ++ []
+
+    flags
   end
 end
